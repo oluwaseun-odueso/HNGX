bddd3e9857570d7946e3be6d9d6dfdd820b6838c
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-09-11 14:31:38 +0100

    UTC issue fixed

--- server.js
+++ server.js
@@ -10,19 +10,37 @@ app.use(express.json())
 
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
 
+function UTC() {
+   const currentUTC = new Date().getTime();
+   const lowerBound = currentUTC - 2000;
+   const upperBound = currentUTC + 2000;
+   const actualUTC = new Date().getTime();
+ 
+   if (actualUTC >= lowerBound && actualUTC <= upperBound) {
+      const iso8601Time = new Date(actualUTC).toISOString().slice(0, -5) + "Z";
+      return iso8601Time;
+      // return new Date(actualUTC);
+   } else {
+     throw new Error('UTC time validation failed');
+   }
+}
+ 
 app.get('/api', async(req, res) => {
-   let slack_name = req.query.slack_name
-   let track = req.query.track
-
-   res.status(200).json({
-      slack_name,
-      current_day: weekday[new Date().getDay()],
-      utc_time: new Date().toISOString().replace(/\.\d+/, '').replace(/(\d{2}:\d{2}:\d{2})Z$/, '$1.00Z'),
-      track,
-      github_file_url: "https://github.com/oluwaseun-odueso/HNGX/blob/main/server.js",
-      github_repo_url: "https://github.com/oluwaseun-odueso/HNGX",
-      status_code: 200
-   })
+   try {
+      const slack_name = req.query.slack_name
+      const track = req.query.track
+      res.status(200).json({
+         slack_name,
+         current_day: weekday[new Date().getDay()],
+         utc_time: UTC(),
+         track,
+         github_file_url: "https://github.com/oluwaseun-odueso/HNGX/blob/main/server.js",
+         github_repo_url: "https://github.com/oluwaseun-odueso/HNGX",
+         status_code: 200
+      })
+   } catch (error) {
+      res.status(500).json({message: error.message})
+   }
 });
 
 app.listen(port, () => {
