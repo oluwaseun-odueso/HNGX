a94bafb63a14fac8ad2398a57339a9c73135a7ad
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-09-11 12:25:28 +0100

    Stage one task

--- server.js
+++ server.js
@@ -4,28 +4,18 @@ const port = 4000
 const app = express();
 app.use(express.json())
 
-var weekday = new Array(7);
-weekday[0]="Monday";
-weekday[1]="Tuesday";
-weekday[2]="Wednesday";
-weekday[3]="Thursday";
-weekday[4]="Friday";
-weekday[5]="Saturday";
-weekday[6]="Sunday";
-console.log("Today is " + weekday[3]);
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
 
-app.get('/', async(req, res) => {
+app.get('/hng', async(req, res) => {
    let slack_name = req.query.slack_name
    let track = req.query.track
-   let utc_time = Date.UTC()
-   let day = new Date().getDay()
 
    res.status(200).json({
       slack_name,
-      current_day: weekday[day],
-      utc_time,
+      current_day: weekday[new Date().getDay()],
+      utc_time: new Date(),
       track,
-      github_file_url: "https://github.com/oluwaseun-odueso/HNGX/server.js",
+      github_file_url: "https://github.com/oluwaseun-odueso/HNGX/blob/main/server.js",
       github_repo_url: "https://github.com/oluwaseun-odueso/HNGX",
       status_code: 200
    })
